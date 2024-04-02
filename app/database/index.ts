a849c80df96a50e50b943ae9d5079da4ac352f42
ajith.woodenclouds <ajith.woodenclouds@gmail.com>
2024-04-03 01:31:40 +0530

    new-databsae

--- app/database/index.ts
+++ app/database/index.ts
@@ -12,7 +12,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI!, {
-      dbName: "Halo",
+      dbName: "Halo_data",
     });
 
     isConnected = true;
